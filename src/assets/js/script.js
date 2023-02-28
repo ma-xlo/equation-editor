d5809ccd88cfa3a0d26730644bfd2fa7286c2bdd
ma-xlo <macorrea-@hotmail.com>
2023-02-28 13:49:35 -0300

    feat: add a clear button to clear input box and the preview area

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,14 +1,14 @@
 const inputBox = document.querySelector('#equation-input')
 const equationPreview = document.querySelector('#equation-preview')
-
+const clearBtn = document.querySelector('#clear-btn');
 
 inputBox.addEventListener('keyup', () => {
-
     inputBox.value = inputBox.value.replaceAll('*', '⋅')
-
     equationPreview.innerHTML = `\\(${inputBox.value}\\)`
     MathJax.Hub.Typeset()
     //MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+})
 
-
+clearBtn.addEventListener('click', () => {
+    equationPreview.innerHTML = inputBox.value = '';
 })
