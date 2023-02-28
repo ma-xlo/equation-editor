9a705da6e7f9638d989dcd9dcd52f4a7bf32edc9
ma-xlo <macorrea-@hotmail.com>
2023-02-28 13:38:06 -0300

    feat: allow update the preview content in real time

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -3,8 +3,12 @@ const equationPreview = document.querySelector('#equation-preview')
 
 
 inputBox.addEventListener('keyup', () => {
-    equationPreview.innerHTML = `\\(${inputBox.value}\\)`
-    MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-})
 
-//\(x = {-b \pm \sqrt{b^2-4ac} \over 2a}\)
+    inputBox.value = inputBox.value.replaceAll('*', '⋅')
+
+    equationPreview.innerHTML = `\\(${inputBox.value}\\)`
+    MathJax.Hub.Typeset()
+    //MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+
+
+})
