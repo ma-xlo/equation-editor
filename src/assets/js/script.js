1d7ee93a9fa7439d22dc00c81c1d6b2fde32410d
ma-xlo <macorrea-@hotmail.com>
2023-02-28 21:34:32 -0300

    feat: add a text editor with support for attachements

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,8 +1,16 @@
 const inputBox = document.querySelector('#equation-input')
 const equationPreview = document.querySelector('#equation-preview')
 const clearBtn = document.querySelector('#clear-btn');
+const editorCloseBtn = document.querySelector("#editor-close-btn")
+const container = document.querySelector('.container');
+const editorBtn =  document.querySelector("#editor-btn")
+const insertBtn = document.querySelector('#insert-btn')
+var element = document.querySelector("trix-editor")
+
+
 
 inputBox.addEventListener('keyup', () => {
+    inputBox.focus();
     inputBox.value = inputBox.value.replaceAll('*', '⋅')
     equationPreview.innerHTML = `\\(${inputBox.value}\\)`
     MathJax.Hub.Typeset()
@@ -11,4 +19,20 @@ inputBox.addEventListener('keyup', () => {
 
 clearBtn.addEventListener('click', () => {
     equationPreview.innerHTML = inputBox.value = '';
+})
+
+editorCloseBtn.addEventListener('click', () => {
+    container.classList.add('hide');
+})
+
+editorBtn.addEventListener('click', () => {
+    container.classList.remove('hide');
+    inputBox.focus();
+})
+
+insertBtn.addEventListener('click', () => {
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+    var attachment = new Trix.Attachment({ content: `${equationPreview.innerHTML}`})
+    element.editor.insertAttachment(attachment)
+    container.classList.add('hide');
 })
